3f1a8c1020cd16098f4348667dfbc9d89f1d3033
Mathias Stadler <stadler-mathias@web.de>
2025-09-29 15:32:02 +0200

    save

--- scripts/test/01_first_letter_variable.sh
+++ scripts/test/01_first_letter_variable.sh
@@ -4,6 +4,9 @@
 # FOUND HERE
 # https://stackoverflow.com/questions/10218474/how-to-obtain-the-first-letter-in-a-bash-variable#10218528
 
+
+# 1759144852061815 
+# 1759144852062444
 variable="1759059933655714";
 
 # four
@@ -21,8 +24,58 @@ result="$((end - start))"
 echo "result => $result"
 echo "length of result ${#result}";
 echo " r=$(echo "$((end - start)) 10000" | awk '{printf "%.2f\n", $1 / $2}' )"
+echo " r=$(echo "$((end - start)) 10000" | awk '{printf "%f\n", $1 / $2}' )"
 
-#wann sind es volle secunden set a cammo
+
+# 1759144852061815 
+# 1759144852062444
+start="1759144852061815";
+  end="1759144852062444";
+
+echo "/W AWK r=$(echo "$((end - start)) 10000" | awk '{printf "%.2f\n", $1 / $2}' )"
+
+
+echo "Length of start $ => ${#start}";
+echo "Length of end $ => ${#end}";
+# FOUND HERE
+# https://linuxsimply.com/bash-scripting-tutorial/loop/for-loop/for-loop-with-variable/
+# a=5;
+# for ((count=1; count<=a; count++)); do
+# echo "Iteration $count"
+# done
+
+a=${#start};
+different=1 # is false
+different_start=()
+different_end=()
+
+for ((i=0; i<=a; i++)); do
+echo "Iter $i"
+sign_start="sign $i ${start:$i:1}";
+echo "$sign_start"
+sign_end="sign $i ${end:$i:1}";
+echo "$sign_end"
+# FOUND HERE
+# https://linuxsimply.com/bash-scripting-tutorial/conditional-statements/if-else/compare-numbers/
+if [[ "$sign_start" = "$sign_end" ]];
+ then
+    echo " Iter = $i Number1 is equal to number2"
+ else
+    echo "Iter = $i Number1 is NOT equal to number2"
+    set_different(1);
+    
+fi
+
+if [[ different -eq 0 ]];
+then
+  different_start+=("$sign_start")
+  different_end+=("$sign_end")
+
+fi
+
+done
+
+#exit 0;
 
 first_sign=${variable:0:1}
 first_sign=${variable::1}
@@ -58,9 +111,6 @@ do
 # add  
 echo "$i";
 echo " sign ${variable:$i:1}"
-clone_array+=("${variable:$i:1}")
+clone_array+=("${variable:$i:1}");
 done
-
-
-echo "clone array ${clone_array[*]}"
 
---
+++ scripts/test/02_first_letter_variable.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/bash
+# shellcheck shell=bash
+
+# FOUND HERE
+# https://stackoverflow.com/questions/10218474/how-to-obtain-the-first-letter-in-a-bash-variable#10218528
+
+
+#
+
+
+
+# FOUND HERE
+# https://linuxsimply.com/bash-scripting-tutorial/loop/for-loop/for-loop-with-variable/
+# a=5;
+# for ((count=1; count<=a; count++)); do
+# echo "Iteration $count"
+# done
+
+a=${#start};
+different=1 # true = 0, false=1
+different_start=()
+different_end=()
+
+
+
+
+function calc_different() {
+
+# in plain bash diff / awk / bc not available
+# $1 start first floating number as string
+# $2 end second floating number as string
+
+start=$1;
+end=$2;
+
+echo "$1"
+echo "$2"
+#1: get the length of strings
+
+len_start="${start}";
+len_end="${end}";
+
+len_start="${#start}";
+len_end="${#end}";
+echo "$len_start"
+echo "$len_end"
+#2: check if have the same length
+if [[ "$len_start" = "$len_end" ]];
+log "I" "Same length"
+then
+log "I" "ERROR Not the same length"
+exit 1;
+fi
+
+
+start=$1
+end=$2
+
+# iter over both strings
+for ((i=0; i<=a; i++)); do
+# echo "Iter $i"
+sign_start="sign $i ${start:$i:1}";
+# echo "$sign_start"
+sign_end="sign $i ${end:$i:1}";
+# echo "$sign_end"
+# FOUND HERE
+# https://linuxsimply.com/bash-scripting-tutorial/conditional-statements/if-else/compare-numbers/
+if [[ "$sign_start" -eq "$sign_end" ]];
+ then
+    log "D" " Iter = $i Digit of a number equal $sign_start :: $sign_end"
+ else
+    echo "Iter = $i Digit of a number NOT equal => write to different array"
+    different=0 # true
+    
+fi
+
+if [[ different -eq 0 ]];
+then
+  # log "I" "set different stelle in der zahl";
+  different_start+=("$sign_start")
+  different_end+=("$sign_end")
+
+fi
+
+done
+
+return 0;
+}
+
+
+TAG="-"
+LOG_FILE="script.log"
+
+function log() {
+# log - useful for script flow level info/debug
+
+	if [ "$HIDE_LOG" ]; then
+		echo -e "[$TAG] $*" >>$LOG_FILE
+	else
+		echo "[$(date +"%Y/%m/%d:%H:%M:%S %z")] [$TAG] $*" | tee -a $LOG_FILE
+	fi
+
+return 0;
+}
+# end function log
+
+
+function run () {
+
+start="1759144852061815";
+  end="1759144852062444";
+
+# calc_different()
+# calc_different(start, end)
+
+log "I" "start";
+calc_different "$start" "$end";
+log "I" "end";
+
+  return 0;
+}
+
+
+function main(){
+run||exit_handler;
+}
+
+main;
+
+# TODO [Running] /usr/bin/bash "/home/trapapa/workspace_codium/rust_init_project/scripts/test/02_first_letter_variable.sh"
+# TODO [Done] exited with code=1 in 0.016 seconds
